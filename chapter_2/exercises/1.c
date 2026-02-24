2cddc97e2d4909ffa51776d3a97ba38cbb74e0cb
theboss <theboss@thebosss-MacBook-Pro.local>
2026-02-24 15:12:36 -0600

    first

---
+++ chapter_2/exercises/1.c
@@ -0,0 +1,8 @@
+// Create and run K & R's hello world program, see if I get a warning message and what is needed to make it disappear
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
