d1b3d2443edaec1118003d226328036b22f9ceb5
theboss <theboss@thebosss-MacBook-Pro.local>
2026-02-24 16:24:27 -0600

    added exercises

---
+++ chapter_2/exercises/2.c
@@ -0,0 +1,20 @@
+//Identify the directives and statements and output does it produce?
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+}
+
+//A) Directives = #include <stdio.h>
+//A) Statements = print statements and return
+
+//B) Prints the following:
+/*
+Parkinson's Law:
+Work expands so as to fill the time
+available for its completion.
+*/
---
+++ chapter_2/exercises/3.c
@@ -0,0 +1,24 @@
+/* Condense the dweight.c program by (1) replacing the assignments to height, length, and width
+ * with initializers and (2) removing the weight variable, instead calculating (volume + 165) / 166
+ * within the last printf.
+*/
+
+// Computes the dimensional weight of a 12" x 10" x 8" box
+
+#include <stdio.h>
+
+int main(void) {
+//    int height, length, width, volume, weight;
+
+    int height = 8;
+    int length = 12;
+    int width = 10;
+    int volume = height * length * width;
+//    int weight = (volume + 165) / 166;
+
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166);
+
+    return 0;
+}
